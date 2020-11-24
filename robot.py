1cf5ccec1b16d51a65f20814f48daac01c8d455e
dannyp99 <dannypires99@gmail.com>
2020-11-23 23:49:45 -0500

    push from the raspberry pi code

--- robot.py
+++ robot.py
@@ -7,7 +7,6 @@ file = open('.env', 'r')
 
 API_TOKEN = file.readline().rstrip()
 zoom_link = file.readline()
-print(zoom_link)
 # - - - - - - - - - - - - - - - -
 # Initialize bot and dispathcer
 bot = Bot(token=API_TOKEN)
@@ -16,7 +15,7 @@ dp  = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def start(message: types.Message):
-	await message.answer("Hello I am Hermes messenger of Gods and this Group Chat\n/start\n/help    see commands\n/zoom   see the meeting time and link")
+    await message.answer("Hello I am Hermes messenger of Gods and this Group Chat\n/start\n/help    see commands\n/zoom   see the meeting time and link")
 
 
 @dp.message_handler(commands=['zoom'])
@@ -24,13 +23,15 @@ async def meet(message: types.Message):
     await message.answer(zoom_link)
 
 async def reminder():
-	try:
-	    await bot.send_message(chat_id=-476860228, text='I have an important message that we are meeting today!\nPlease join here:\n' + zoom_link)
-	except Exception:
-		traceback.print_exc()
+    try:
+        await bot.send_message(chat_id=-476860228, text='Danny is an idiot programmer.')
+        #await bot.send_message(chat_id=-476860228, text='I have an important message that we are meeting today!\nPlease join here:\n' + zoom_link)
+
+    except Exception:
+        traceback.print_exc()
 
 loop = asyncio.get_event_loop()
 loop.create_task(reminder())
 
 if __name__ == '__main__':
-	executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)
